00c6a299c7b118321814c9df5af03cf386720261
Le0z1nk <leoamorim132@gmail.com>
2023-03-03 10:59:25 -0300

    finalizando atividade

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
+const GlobalStyle = createGlobalStyle`
+body {
+  background-color: palegreen
 }
+`
+const Secao = styled.section`
+width: 50%;
+display: flex;
+flex-direction: column;
+margin-left: 19rem;
+margin-top: 10rem;
+text-align: center;
+border: solid black;
+background-color: aquamarine;
+`
+const Title = styled.h1`
+color: blue;
+font-size: 4rem;
+`
+const Divisao = styled.div`
 
-export default App;
+display: flex;
+justify-content: center;
+justify-content: space-evenly;
+`
+const Botao = styled.button`
+width: 9vw;
+height: 8vh;
+border-radius: 20vh;
+font-size: 2rem;
+background-color: blue;
+`
+class App extends Component {
+  state = {
+    numero: 0 
+  }
+  adicionar = () => {
+    if(this.state.numero < 10){
+      this.setState({
+        numero: this.state.numero + 1
+      })
+    }
+  }
+  remover = () => {
+    if(this.state.numero > 0){
+      this.setState({
+        numero: this.state.numero - 1
+      })
+    }
+  }
+  render() {
+    return(
+      <>
+      <GlobalStyle paleGreenColor />
+      <Secao>
+      <Title>{this.state.numero} </Title>
+      <Divisao>
+<Botao onClick={this.adicionar}> + </Botao>
+<Botao onClick={this.remover}> - </Botao>
+</Divisao>
+</Secao>
+      </>
+    )
+  }
+}
+export default App
